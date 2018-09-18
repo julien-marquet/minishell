cbdd6c8ee936e336540c1379d7c1e20eecd27f8e
Julien Marquet <jmarquet@z2r4p4.le-101.fr>
2018-09-18 19:09:57 +0200

    Cut input

--- main.c
+++ main.c
@@ -6,22 +6,48 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/09/18 16:45:54 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/18 18:16:42 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/18 19:09:21 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	parse_input(char *str_i, char **tokens)
+char	*process_token(char *str_i, unsigned char *in_word,
+size_t start, size_t i)
 {
-	ssize_t	i;
+	*in_word = 0;
+	ft_printf("%zu, %zu\n", start, i);
+	return (NULL);
+}
+
+int		parse_input(char *str_i, char **tokens)
+{
+	unsigned char	in_word;
+	size_t			ntoken;
+	unsigned char	is_quoted;
+	size_t			start;
+	size_t			i;
 
 	i = 0;
-	while (i)
+	start = 0;
+	ntoken = 0;
+	in_word = 0;
+	is_quoted = 0;
+	while (str_i[i++])
 	{
-		i++;
+		if (is_metachar(str_i[i]) && !is_quoted)
+		{
+			if (in_word > 0 && (tokens[ntoken] = process_token(str_i,
+		&in_word, start, i - start)) == NULL)
+				return (1);
+		}
+		else if (str_i[i] == '\'')
+			is_quoted = !is_quoted;
+		else if (in_word == 0 && ++ntoken && ++in_word)
+			start = i;
 	}
+	return (0);
 }
 
 size_t	count_tokens(char *str_i)
@@ -63,7 +89,9 @@ int     main(void)
 		buf[ret] = '\0';
 		if ((tokens = allocate_tokens(count_tokens(buf))) == NULL)
 			return (1);
-		parse_input(buf, tokens);
+		if (parse_input(buf, tokens) != 0)
+			return (1);
+
 	}
 	/*
 	**  PARSING
