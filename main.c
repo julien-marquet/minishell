b332464c238862879b4b93597bcd26e4875b17c3
Julien Marquet <jmarquet@z2r3p4.le-101.fr>
2018-09-28 19:18:46 +0200

    Clean

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/09/18 16:45:54 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/28 05:29:07 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/28 18:58:29 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,26 +23,54 @@ static void		handle_error(char **err)
 	}
 }
 
+static char		*handle_read(void)
+{
+	size_t	len;
+	char	*tmp;
+	ssize_t ret;
+	char	buf[BUF_SIZE + 1];
+	char	*res;
+
+	res = NULL;
+	ret = BUF_SIZE + 1;
+	while (ret >= BUF_SIZE)
+	{
+		len = 0;
+		ret = read(0, buf, BUF_SIZE);
+		buf[ret] = '\0';
+		if (ret == -1 || (tmp = ft_memalloc(len + ret + 1)) == NULL)
+			return (NULL);
+		if (ret == 0)
+			break ;
+		if (res != NULL)
+			ft_strcpy(tmp, res);
+		ft_strcpy(&(tmp[len]), buf);
+		ft_strdel(&res);
+		len += ret;
+		res = tmp;
+	}
+	return (res);
+}
+
 static int		handle_input(char **tokens, int *status, char ***env)
 {
-	char	buf[BUF_SIZE + 1];
-	ssize_t ret;
 	int		sig_exit;
 	char	*err;
+	char	*str;
 
 	err = NULL;
+	str = NULL;
 	ft_putstr("$> ");
-	ret = read(0, buf, BUF_SIZE);
-	if (ret == -1)
+	if ((str = handle_read()) == NULL)
 		return (1);
-	buf[ret] = '\0';
-	if ((tokens = allocate_tokens(count_tokens(buf))) == NULL)
+	if ((tokens = allocate_tokens(count_tokens(str))) == NULL)
 		return (1);
-	if (parse_input(buf, tokens, *env) != 0)
+	if (parse_input(str, tokens, *env) != 0)
 		return (1);
 	sig_exit = dispatch_commands(tokens, env, &err, status);
 	free_array_str(tokens);
 	handle_error(&err);
+	ft_strdel(&str);
 	return (sig_exit);
 }
 
